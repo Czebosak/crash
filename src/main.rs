681ba4b1890caaf4548d6efb88a659b1d0a6ca58
Czebosak <czebosak@proton.me>
2024-06-02 21:15:04 +0200

    Added error handling and path formatting

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,55 @@ use std::env;
 
 mod prompt;
 
+fn main_loop() {
+    prompt::print_prompt();
+    
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+   
+    if input.trim().is_empty() {
+        return;
+    }
+
+    // read_line leaves a trailing newline, which trim removes
+    let mut parts = input.trim().split_whitespace();
+    let command = parts.next().unwrap();
+
+    let args = parts;
+
+    match command {
+        "cd" => {
+            let new_dir = args.peekable().peek().map_or("/", |x| *x);
+
+            let home_dir: String;
+            match home::home_dir() {
+                Some(x) => home_dir = prompt::path_to_string(x.as_path()),
+                None => home_dir = "/".to_string(),
+            }
+
+            let new_dir2 = new_dir.replace("~", &home_dir).to_string();
+
+            let root = Path::new(&new_dir2);
+            if let Err(e) = env::set_current_dir(root) {
+                eprintln!("{}", e);
+            }
+        },
+        "exit" => std::process::exit(0),
+        command => {
+            let child = Command::new(command)
+                .args(args)
+                .spawn();
+
+            match child {
+                Ok(mut child) => { let _ = child.wait(); },
+                Err(e) => eprintln!("{}", e),
+            };
+        }
+    }
+}
+
 fn main(){
     loop {
-        prompt::print_prompt();
-
-        let mut input = String::new();
-        stdin().read_line(&mut input).unwrap();
-
-        // read_line leaves a trailing newline, which trim removes
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
-
-        match command {
-            "cd" => {
-                // default to '/' as new directory if one was not provided
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
-                }
-            },
-            "exit" => return,
-            command => {
-                let mut child = Command::new(command)
-                    .args(args)
-                    .spawn()
-                    .unwrap();
-
-                let _ = child.wait();
-            }
-        } 
+        main_loop();
     }
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,14 +1,52 @@
 use std::io::{stdout, Write};
 use std::env;
+use std::path::Path;
 
-fn path_buf_to_string(path: std::path::PathBuf) -> String {
-    return path.into_os_string().into_string().unwrap().trim_matches('"').to_string();
+pub fn path_to_string(path: &Path) -> String {
+    return path.to_str().unwrap().trim_matches('"').to_string();
+}
+
+fn generate_prompt() {
+    let max_dir_length: usize = 3;
+    let prompt_symbol = '❯';
+    let current_dir = env::current_dir().unwrap();
+    
+    let dir: &Path;
+    let dir_components = current_dir.components();
+    let components_count = dir_components.clone().count();
+
+    //match home::home_dir() {
+    //    Some(x) => {
+    //        let home_dir = path_to_string(x.as_path());
+    //        let current_dir_str = path_to_string(&current_dir);
+
+    //        match current_dir_str.strip_prefix(&home_dir) {
+    //            Some(stripped_dir) => current_dir = "~".to_owned().push_str(&stripped_dir),
+    //            None => ()
+    //        }
+    //    },
+    //    None => (),
+    //}
+
+    if components_count > max_dir_length.into() {
+        let mut components_iter = dir_components;
+        for _ in 0..(components_count - max_dir_length) {
+            components_iter.next();
+        }
+        dir = components_iter.as_path();
+    }
+    else {
+        dir = current_dir.as_path();
+    }
+
+    print!("{} {} ", path_to_string(dir), prompt_symbol);
 }
 
 pub fn print_prompt() {
-    let prompt_symbol = '❯';
-    print!("{} {} ", path_buf_to_string(env::current_dir().unwrap()), prompt_symbol);
+    //print!("{} {} ", path_to_string(env::current_dir().unwrap()), prompt_symbol);
     
+    generate_prompt();
+
     match stdout().flush() {
         Ok(_) => (),
         Err(error) => println!("{}", error),
